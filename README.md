41287dab2b2b07b36057d340dceb1bcbea4dbc22
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2021-04-17 15:15:06 -0300

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,36 @@
+# Linkedin Webscraping
+
+## Descrição
+
+Programa para realizar WebScraping em perfils do linkedin, retornando informações como Nome, Empresa ETC. Utilizando Selenium & Selector.
+
+## Como Usar
+
+### Instalando
+Clone este repositorio:
+
+```git clone https://github.com/kilerhg/linkedin_webscraping.git```
+
+Entre no repositorio:
+```cd linkedin_webscraping```
+
+Instalar Requisitos:
+```pip install -r requirements.txt```
+
+## Abrindo
+
+Abrir o Cmd ou PowerShell Na pasta e rodar o comando: ```python main.py```
+
+## Task-List
+
+- [X] Desenvolver a ideia
+- [X] Logar No linkedin
+- [X] Entra na Pagina desejada
+- [X] Achar Parâmetros nome,titulo,empresa,escola,link_perfil
+- [X] Salvar em Variáveis
+- [X] Criar funções que gravam em CSV
+
+## Biliografia
+
+> * [Checando Existencia Arquivo](https://linuxize.com/post/python-check-if-file-exists/)
+> * [Documentação CSV](https://docs.python.org/3/library/csv.html)
---
+++ main.py
@@ -0,0 +1,119 @@
+import csv
+from time import sleep
+from selenium import webdriver
+from parsel import Selector
+from selenium.webdriver import Firefox
+
+def SalvarCsv(nome,titulo,empresa,escola,link,nome_arquivo='base'):
+    try:
+        f = open(f"{nome_arquivo}.csv") # Verifica Existencia
+        with open(f'{nome_arquivo}.csv','a+', encoding='utf-8') as arquivo: # Define Atalho como arquivo
+            arquivo.write(f'{nome},{titulo},{empresa},{escola},{link}\n') # Salva as informações
+
+    except IOError: # Caso não exista arquivo cai nesta exeção
+        with open(f'{nome_arquivo}.csv','w+', encoding='utf-8') as arquivo:
+            arquivo.write('nome,titulo,empresa,escola,link\n') # Salva Cabeçalho
+            arquivo.write(f'{nome},{titulo},{empresa},{escola},{link}\n') # Salva Dados concatenando
+
+    arquivo.close() # Fecha arquivo
+
+# aqui é feita a exigência das urls dos perfis
+#input_url = str(input('urls: ')) # aqui é feita a requisição dos urls
+#linkedin_url = input_url.split("https://") # aqui é feita a divisão dos urls pelo https
+#linkedin_url.remove(lista_url[0]) # o primeiro item da lista fica vazio e por isso o tirei
+linkedin_urls =  ['https://www.linkedin.com/in/andr%C3%A9-felipe-guisasola-antunes-9a0490173/'] #url de teste
+#for i in range(len(linkedin_url)): # for loop pra completar cada item da lista com o restante que faltava da url
+#    url_completa="https://" + linkedin_url[i]
+#    linkedin_url_url[i]=url_completa
+########## Urls armazenadas ##########
+
+
+########## Armazenando Usuario e senha ##########
+usuario = input('Digite usuario: ')
+senha = input('Digite senha: ')
+########## Armazenando Usuario e senha ##########
+
+
+
+# nessa etapa inicial o webdriver é aberto no diretório abaixo
+driver = Firefox()
+# nessa etapa é aberto o linkedin via webdriver
+driver.get('https://www.linkedin.com')
+# encontra a categoria de e-mail
+
+# Achando Campo Usuario
+username = driver.find_element_by_id('session_key')
+
+# Envia senha
+username.send_keys(f'{usuario}')
+
+# dormida de 0.5 segundos
+sleep(1.0)
+
+# encontra categoria de senha
+password = driver.find_element_by_id('session_password')
+
+# Enviar Senha
+password.send_keys(f'{senha}')
+
+# dormida de 1 segundo
+sleep(1.0)
+
+# localiza-se o botão de entrar
+log_in_button = driver.find_element_by_class_name('sign-in-form__submit-button')
+
+#clia-se no botão
+log_in_button.click()
+
+########## DENTRO DO LINKEDIN ##########
+from parsel import Selector
+# faz-se o loop de iteração em cada url da lista de url
+for linkedin_url in linkedin_urls:
+    driver.get(linkedin_url) # o perfil da pessoa é acessado
+    sleep(5) # tempo de espera de 5 segundos
+    sel = Selector(text=driver.page_source) # coleta-se o código fonte da página daquele perfil
+    name = sel.xpath('//div[@class="flex-1 mr5 pv-top-card__list-container"]/ul/li/text()').extract_first() # Coleta Nome
+    
+    # Limpa Variável name
+    if name:
+        name = name.strip() # Tira Espaços Vazios Antes e depois
+        print(name)
+    else:
+        name = 'Nome não encontrado' # caso de erro retorna para Variável
+
+    job_title = sel.xpath('//div[@class="flex-1 mr5 pv-top-card__list-container"]/h2/text()').extract_first() # Coleta Titulo
+
+    # Limpa Variável Titulo
+    if job_title:
+        job_title = job_title.strip() # Tira Espaços Vazios Antes e depois
+        print(job_title)
+    else:
+        job_title = 'Trabalho não encontrado' # caso de erro retorna para Variável
+
+    company_college = sel.xpath('//ul[@class="pv-top-card--experience-list"]/li/a/span/text()').getall() # Coleta Escola & Empresa
+
+    # Limpa Variável Escola & Empresa
+    if len(company_college) > 1:
+        # Caso a pessoa tenha trabalho puxa ambos 
+        company = company_college[0] # Separando Variaveis
+        college = company_college[1] # Separando Variaveis
+    else:
+        # Caso a pessoa não tenha trabalho puxa somente Escola
+        company = '' # Deixa Variável vazia 
+        college = company_college[0] # Separando Variaveis
+
+    if company:
+        company = company.strip() # Tira Espaços Vazios Antes e depois
+    else:
+        company = 'Empresa não encontrada' # caso de erro retorna para Variável
+
+    if college:
+        college = college.strip() # Tira Espaços Vazios Antes e depois
+        print(college)
+    else:
+        college = 'Acadêmico não encontrado' # caso de erro retorna para Variável
+
+    linkedin_url = driver.current_url # Pegando Link do Perfil Atual
+
+    SalvarCsv(name,job_title,company,college,linkedin_url) # Executando Função para salvar os dados
+driver.quit() # fecha-se o driver
